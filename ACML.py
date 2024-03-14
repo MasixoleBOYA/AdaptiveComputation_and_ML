3c92946b426f1b1f0018aa52521c8c566d9708d2
alfred khanyile <mr.khanyile21@gmail.com>
2024-03-14 17:47:04 +0000

    .

--- ACML.py
+++ ACML.py
@@ -7,16 +7,20 @@ D = {((3, 1), 1),
      ((2, 2.5), 0), 
       ((2, 1.5), 1), 
       ((4, 3), 1), 
-      ((3, 3), 0)},
+      ((3, 3), 0)}
 
-# Variables
-weights = [2,3.5,6]
+# parameters randomly assigned
+weights = [2,3.5]
 learning_rate = 0.01
 threshold = -1
+# inupts and targets
+inputs = [i[0] for i in D]
+targets = [j[1] for j in D]
 
+print(f"INPUTS: \n{inputs}")
+print(f"\nTARGETS: \n{targets}")
 
 array_of_inputs = []
 targets =  []
 
-for i in D:
-    print(f"i: {i}")
+
