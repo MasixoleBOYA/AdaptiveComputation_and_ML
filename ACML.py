7fdbe533626228152f49e8bbdbd37f93cdb50745
MasixoleBoya <tshollyboya00@gmail.com>
2024-03-19 22:13:03 +0000

    .

--- ACML.py
+++ ACML.py
@@ -18,26 +18,67 @@ threshold:int = -1
 inputs:list = [i[0] for i in D]
 targets:list = [j[1] for j in D]
 
-print(f"INPUTS: \n{inputs}")
+print(f"\nINPUTS: \n{inputs}")
 print(f"\nTARGETS: \n{targets}")
 
+
+
+
+class SimplePerceptron:
+    '''
+    Expects threshold value, a list weight values, lis of input values
+    Requirement: the list of wieghts should be of the same length as the tuples (which represent the datapoints) 
+    '''
+    def __init__(self, thresholdValue:int, weightValues:list, inputValues:list) -> None:
+        self.threshold = thresholdValue
+        self.weights = weightValues
+        self.inputs = inputValues
+
+    def perceptron(self):
+        h = 0
+        for data_point in inputs:
+            # print(f"The current data point for the class :\n{data_point} ")
+            for actualInput, corresponding_Weight in zip(data_point, weights):
+                h = h + actualInput*corresponding_Weight
+                # print(f"H: {h}")
+            
+        if h > threshold:
+            return 1
+        elif h <= threshold:
+            return 0
+                
+        
 def perceptronTrainingAlgorithm(tresholdValue: int, learningRateValue:int , weightValues: list, targetValues: list, inputValues:list):
-    summation = (inputValues[0]*weightValues[0])+(inputValues[1]*weightValues[1])
+    '''
+    inputValues: the list of inputs
+    weightValues: the list of weights
+    '''
+    summation = (inputValues[0]*weightValues[0]) + (inputValues[1]*weightValues[1])
     if summation>tresholdValue:
         output = 1
     elif summation < tresholdValue:
         output = 0
 
-    return output       
+    return output
     
     updatedThreshold = tresholdValue - (learningRateValue*(output-trueValue))
 
 
-epoch = 0
-while epoch < 100:
-    for input in inputs:
-        print(epoch)
-        epoch+=1
+if __name__ == "__main__":
+
+    testing = SimplePerceptron(thresholdValue= threshold, 
+                               weightValues= weights, 
+                               inputValues= inputs)
+
+    checking = testing.perceptron()
+    print(f"\OUTPUT of Perceptron is: {checking}")
+
+# epoch = 0
+# while epoch < 10:
+#     for dataPoint in inputs:
+#         print(f"epoch: {epoch}")
+#         print(f"Data point: {dataPoint}")
+#         epoch+=1
      
 
 
---
+++ practise.py
@@ -0,0 +1,6 @@
+list1 = ["a","b","c","d"]
+list2 = [1,2,3,4]
+
+for i,j in zip(list1,list2):
+    print(f"i: {i}")
+    print(f"j: {j}\n")
